fc82fb954d058e9eb081ae5a032e021d353e4bf1
kumar-harin <haryn.k10@gmail.com>
2022-03-27 17:04:22 +0530

    adding code for compensator designs and digitization of the entire system

--- apc.m
+++ apc.m
@@ -5,7 +5,7 @@
 % theta - pitch angle
 % delta - elevator deflection angle
 
-% input : delta
+% input: delta
 % output: theta
 % ----------------------------------
 
@@ -24,21 +24,31 @@
 % ss error < 2%
 % ----------------------------------
 
-% using pid controller - easiest way
+% transfer function
 s = tf('s');
 p_pitch = (1.151 * s + 0.1774) / (s ^ 3 + 0.739 * s ^ 2 + 0.921 * s);
-controlSystemDesigner(p_pitch); % Kp = 5.1852, Ki = 1.7, Kd = 2.98
 
-% using lqr algorithm
+% state space model
 % x = [alpha q theta]
 % u = [delta]
+% y = [theta]
 A = [-0.313 56.7 0; -0.0139 -0.426 0; 0 56.7 0];
 B = [0.232; 0.0203; 0];
 C = [0 0 1];
 D = [0];
 ss_pitch = ss(A, B, C, D);
-co = rank(ctrb(A, B))
 
+% using pid controller: the easiest way
+controlSystemDesigner(p_pitch); % Kp = 5.1852, Ki = 1.7, Kd = 2.98
+
+% using compensator-rootlocus method
+controlSystemDesigner(p_pitch); % lead compensator: 200 * (s + 0.9) / (s + 30)
+
+% using compensator-bode method
+controlSystemDesigner(p_pitch); % lead compensator: 10 * (0.55 * s + 1) / (0.022 * s + 1) with K = 10, alpha = 0.04, T = 0.55
+
+% using lqr algorithm
+co = rank(ctrb(A, B))
 p = 50;
 Q = p * C' * C;
 R = 1;
@@ -48,3 +58,16 @@ Nbar = 1 / dcgain(ss_cl);
 ss_cl = ss(A - B * K, Nbar * B, C, D);
 step(ss_cl);
 ylabel('pitch angle(rad)');
+
+% digitization
+Ts = 1 / 30;
+Kp = 5.1852;
+Ki = 1.7;
+Kd = 2.98;
+c = pid(Kp, Ki, Kd); % this can also be a lead compensator
+dc = c2d(c, Ts, 'tustin');
+dp = c2d(p_pitch, Ts, 'zoh');
+sys = feedback(c * p_pitch, 1);
+d_sys = feedback(dc * dp, 1);
+step(sys, d_sys);
+legend('continuous', 'digitized');
